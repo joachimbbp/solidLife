eadd47b3472f63a6f45397749883bc21acfe7318
joachimbbp <104856283+joachimbbp@users.noreply.github.com>
2025-04-23 10:45:36 -0400

    clock cycles implemented

--- src/app.tsx
+++ src/app.tsx
@@ -1,55 +1,61 @@
 import { MetaProvider, Title } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
-import { Suspense } from "solid-js";
 import "./app.css";
 
-import { createSignal, For, onCleanup, onMount } from "solid-js";
+import { createSignal, For, onCleanup, onMount, createEffect } from "solid-js";
 
 
 //■
-function Clock(){
-  const [time, setTime] = createSignal(new Date());
-  onMount(() => {
-    const interval = setInterval(() =>{
-      setTime(new Date());
-    }, 1000);
-    onCleanup(() => clearInterval(interval));
-  });
-  return <div>{time().toLocaleTimeString()}</div>
-}
-
-
-export default function App() {
+function Life(){
   const rows = 50;
   const cols = 100;
   
   const initialGrid = Array.from({ length: rows }, () =>
     Array.from({length: cols}, () => false));
-  
-  
+
   const [grid, setGrid] = createSignal<boolean[][]>(initialGrid);
+
+  //const [seconds, setSeconds] = createSignal(new Date().getSeconds());
+  onMount(() => {
+    const interval = setInterval(() =>{
+      const nextGrid = grid().map(row => [...row])
+      nextGrid[25][50] = !nextGrid[25][50];
+
+      setGrid(nextGrid);
+    }, 1000);
+    onCleanup(() => clearInterval(interval));
+  });
+  
+    return (
+          <pre style={{"font-family":"monospace",
+            "white-space":"pre",
+            "line-height": "1em",
+            "font-size":"16px"}}>
+          <For each={grid()}>
+            {row =>(
+              <>
+              {row.map(cell => (cell ? "■" : "0")).join("")}
+              {"\n"}
+              </>
+            )}
+          </For>
+          </pre>
+    );
+}
+
+
+export default function App() {
   return (
     <Router
-      root={props=>(
-        <MetaProvider>
-        <Title>Test</Title>
-        <pre style={{"font-family":"monospace",
-          "white-space":"pre",
-          "line-height": "1em",
-          "font-size":"16px"}}>
-            <div>Current count: {Clock()} </div>
-        <For each={grid()}>
-          {row =>(
-            <>
-            {row.map(cell => (cell ? "■" : "0")).join("")}
-            {"\n"}
-            </>
-          )}
-        </For>
-        </pre>
-        </MetaProvider>
-      )}>
+    root={(props) => (
+      <MetaProvider>
+      <Title>Test</Title>
+      <Life />
+      </MetaProvider>
+    )}
+    >
+      <FileRoutes />
     </Router>
   );
 }
